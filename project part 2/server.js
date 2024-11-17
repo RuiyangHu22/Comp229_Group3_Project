4708ee9a70e463643d0b3257d3bd1d96eb77329b
RuiyangHu22 <rhu22@my.centennialcollege.ca>
2024-11-17 07:28:03 -0500

    Part 2

---
+++ project part 2/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')    
+
+const port = 3000;
+
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const booksRouter = require('./routes/books')
+app.use('/books', booksRouter)
+
+app.get('/', (req, res) => {
+    res.send('Welcome to our library');
+});
+
+app.listen(port, () => console.log('Server Started'))
